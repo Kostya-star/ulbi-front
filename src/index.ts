90ffd44504ea003f7253054e3186bc0291135080
Kostya-star <konstantindanilov19@hotmail.com>
2024-07-07 21:17:11 +0300

    create project. add webpack. add ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { testFunc } from './test';
+
+testFunc(55555555);
+
+console.log('23456787654321234565432343');
---
+++ src/test.ts
@@ -0,0 +1,3 @@
+export function testFunc(num: number) {
+  console.log(num);
+}
