21554ca84ed5e23af3f8e3ee24a7230c39a464ff
Kostya-star <konstantindanilov19@hotmail.com>
2024-07-12 21:52:14 +0300

    feat: create black\light theme with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
-import './index.scss';
+import './styles/index.scss';
 import { Link } from 'react-router-dom';
 import { MainPageAsync } from './pages/MainPage/MainPageAsync';
 import { AboutPageAsync } from './pages/AboutPage/AboutPageAsync';
 import { Suspense } from 'react';
+import { useTheme } from './theme/useTheme';
 
 export function App() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <Link to="/">Main</Link>
       <Link to="/about">About</Link>
+      <button onClick={toggleTheme}>change theme</button>
       <Suspense fallback={<div>page is loading...</div>}>
         <Routes>
           <Route path="/" element={<MainPageAsync />} />
