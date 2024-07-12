3bf8495301f64a33bd0b702c7d715a7e520b5c46
Kostya-star <konstantindanilov19@hotmail.com>
2024-07-12 17:27:46 +0300

    feat: add routing, lazy, suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { Counter } from './components/Counter';
-import './index.scss'
+import { Route, Routes } from 'react-router-dom';
+import './index.scss';
+import { Link } from 'react-router-dom';
+import { MainPageAsync } from './pages/MainPage/MainPageAsync';
+import { AboutPageAsync } from './pages/AboutPage/AboutPageAsync';
+import { Suspense } from 'react';
 
 export function App() {
   return (
-    <div className='app'>
-      <Counter />
+    <div className="app">
+      <Link to="/">Main</Link>
+      <Link to="/about">About</Link>
+      <Suspense fallback={<div>page is loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -15,5 +15,5 @@ export function Counter() {
         DECREMENT
       </button>
     </div>
-  );
+  ); 
 }
