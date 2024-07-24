7290e8b70d6c3714c70bbe563ec5c7737c41f040
Kostya-star <konstantindanilov19@hotmail.com>
2024-07-24 15:11:03 +0300

    fix tests for sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,18 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/Sidebar/index';
-
-import {
-  renderWithTranslation,
-} from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { renderComponent } from 'shared/lib/tests/renderComponent/renderComponent';
 
 describe('Sidebar', () => {
   test('to be in the document', () => {
-    renderWithTranslation(<Sidebar />);
+    renderComponent(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('test sidebar width collapsing', () => {
-    renderWithTranslation(<Sidebar />);
+    renderComponent(<Sidebar />);
     const sidebarEl = screen.getByTestId('sidebar');
     expect(sidebarEl).not.toHaveClass('collapsed');
 
