a0ec07b282395a745d4e5a9cff61fa12a8077415
Kostya-star <konstantindanilov19@hotmail.com>
2024-08-14 21:57:29 +0300

    add another 3rd theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -10,8 +10,25 @@ export function useTheme(): UseThemeResult {
   const { theme, setTheme } = useContext(ThemeContext);
 
   const toggleTheme = () => {
-    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
-    // document.body.className = newTheme
+    let newTheme: Theme;
+
+    switch (theme) {
+      case Theme.DARK:
+        newTheme = Theme.LIGHT;
+        break;
+
+      case Theme.LIGHT:
+        newTheme = Theme.DUSK_SERENITY;
+        break;
+
+      case Theme.DUSK_SERENITY:
+        newTheme = Theme.DARK;
+        break;
+
+      default:
+        newTheme = Theme.LIGHT;
+        break;
+    }
     setTheme?.(newTheme);
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
   };
--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,8 +2,9 @@ import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+import DuskSerenityIcon from 'shared/assets/icons/dusk_serenity.svg';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 
 interface ThemeSwitcherProps {
   className?: string;
@@ -12,13 +13,21 @@ interface ThemeSwitcherProps {
 export const ThemeSwitcher = memo(({ className }: ThemeSwitcherProps) => {
   const { theme, toggleTheme } = useTheme();
 
+  const ThemeSwitcherIcon = useMemo(() => {
+    if (theme === Theme.DARK) return DarkIcon;
+    if (theme === Theme.LIGHT) return LightIcon;
+    if (theme === Theme.DUSK_SERENITY) return DuskSerenityIcon;
+
+    return LightIcon;
+  }, [theme]);
+
   return (
     <Button
       theme={ButtonTheme.CLEAR}
       className={classNames('', {}, [className])}
       onClick={toggleTheme}
     >
-      {theme === Theme.LIGHT ? <LightIcon /> : <DarkIcon />}
+      <ThemeSwitcherIcon />
     </Button>
   );
 });
