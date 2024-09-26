c6e3d69945f5e7744d79672c00ea9017ebe031d0
Kostya-star <konstantindanilov19@hotmail.com>
2024-09-26 22:49:43 +0300

    add icon as link

--- src/entities/Notification/ui/NotificationItem/NotificationItem.tsx
+++ src/entities/Notification/ui/NotificationItem/NotificationItem.tsx
@@ -1,10 +1,12 @@
-import { memo, ReactNode, useMemo } from 'react';
+import { memo } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink } from 'shared/ui/AppLink/AppLink';
 import { Avatar } from 'shared/ui/Avatar/Avatar';
 import { Card, CardTheme } from 'shared/ui/Card/Card';
 import { HStack } from 'shared/ui/Stack';
 import { Text, TextSize } from 'shared/ui/Text/Text';
+import UpRightFromSquareIcon from 'shared/assets/icons/up-right-from-square-solid.svg';
+import { Icon } from 'shared/ui/Icon/Icon';
 import type { NotificationItem as INotificationItem } from '../../model/types/notification';
 import cls from './NotificationItem.module.scss';
 
@@ -14,7 +16,7 @@ interface NotificationItemProps {
 }
 
 export const NotificationItem = memo(({ className, notification }: NotificationItemProps) => {
-  const notificationCard = useMemo(() => (
+  return (
     <Card
       className={classNames(cls.NotificationItem, {}, [className])}
       theme={CardTheme.OUTLINE}
@@ -27,20 +29,19 @@ export const NotificationItem = memo(({ className, notification }: NotificationI
           <Avatar src={notification.user.avatar} size={25} />
         </HStack>
       </HStack>
+      {
+        notification.href
+          ? (
+            <AppLink
+              target='_blank'
+              to={notification.href}
+              className={cls.link}
+            >
+              <Icon Svg={UpRightFromSquareIcon} className={cls.linkIcon} />
+            </AppLink>
+          )
+          : null
+      }
     </Card>
-  ), [className, notification.description, notification.title, notification.user.avatar, notification.user.username]);
-
-  if (notification.href) {
-    return (
-      <AppLink
-        target='_blank'
-        to={notification.href}
-        className={classNames(cls.NotificationItem, {}, [className])}
-      >
-        {notificationCard}
-      </AppLink>
-    );
-  }
-
-  return notificationCard;
+  );
 });
