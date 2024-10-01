c02944672e97a1faa2c4b48ac31bca115bfa5667
Kostya-star <konstantindanilov19@hotmail.com>
2024-10-01 12:23:45 +0300

    add vite for dev

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from "vite-plugin-svgr";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({ exportAsDefault: true }),
+  ],
+  define: {
+    __IS_DEV__: JSON.stringify(true),
+    __API_URL__: JSON.stringify('http://localhost:8000'),
+    __PROJECT__: JSON.stringify('frontend'),
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
