2954cc95b3090477d2e83463e114ad2ae0ee1d89
Kostya-star <konstantindanilov19@hotmail.com>
2024-10-08 19:02:59 +0300

    add storybook-addon-themes

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -13,11 +13,19 @@ export const parameters = {
       date: /Date$/,
     },
   },
-  layout: 'fullscreen'
+  layout: 'fullscreen',
+  themes: {
+    default: 'dusk_serenity',
+    list: [
+      {name: 'light', class: ['app', Theme.LIGHT], color: '#ffffff'},
+      {name: 'dark', class: ['app', Theme.DARK], color: '#000000'},
+      {name: 'dusk_serenity', class: ['app', Theme.DUSK_SERENITY], color: 'gray'}
+    ]
+  }
 };
 
 // global decorator for all stories. by default, all stories would have the light theme
-addDecorator((StoryComp) => ThemeDecorator(StoryComp, Theme.LIGHT));
+// addDecorator((StoryComp) => ThemeDecorator(StoryComp, Theme.LIGHT));
 
 // use react router dom
 addDecorator(RouterDecorator);
