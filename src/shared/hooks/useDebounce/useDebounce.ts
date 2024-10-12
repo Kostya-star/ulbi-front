0374c681386e635cb7598a44b794ec10d8b59251
Kostya-star <konstantindanilov19@hotmail.com>
2024-10-12 22:25:36 +0300

    add js doc comments for useDebounce

--- src/shared/hooks/useDebounce/useDebounce.ts
+++ src/shared/hooks/useDebounce/useDebounce.ts
@@ -1,5 +1,24 @@
 import { useCallback, useRef } from 'react';
 
+/**
+ * Custom React hook that returns a debounced version of a callback function.
+ * The callback will only be invoked after the specified delay time has passed
+ * since the last time the debounced function was called.
+ *
+ * @param {Function} cb - The callback function to be debounced.
+ * @param {number} delay - The delay in milliseconds to wait before invoking the callback.
+ *
+ * @returns {Function} A debounced version of the callback function that delays invoking
+ * the callback until after the specified delay time has passed.
+ *
+ * @example
+ * const handleResize = useDebounce(() => {
+ *   console.log('Window resized');
+ * }, 300);
+ *
+ * window.addEventListener('resize', handleResize);
+ */
+
 export const useDebounce = (cb: (...args: any[]) => void, delay: number) => {
   const timeout = useRef<NodeJS.Timeout | null>(null);
 
